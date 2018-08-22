fe6f7cdd65babe89b0985444d591ac599f3ca83a
Okyza Maherdy Prabowo <thesmartmotion@gmail.com>
2018-08-22 19:59:05 +0700

    edit config.php

--- application/config/config.php
+++ application/config/config.php
@@ -14,7 +14,7 @@
 | path to your installation.
 |
 */
-$config['base_url']	= "https://".$_SERVER['HTTP_HOST'].
+$config['base_url']	= "http://".$_SERVER['HTTP_HOST'].
 str_replace(basename($_SERVER['SCRIPT_NAME']),"",$_SERVER['SCRIPT_NAME']);
 
 
